44db187d14ca2ade61e14207959837c2b5ea448c
RuhulRuhul <ruhulamin122017@gmail.com>
2021-06-23 14:41:36 +0600

    ruhul name added

--- app/src/main/java/com/ruhul/teamproject_practice/MainActivity.java
+++ app/src/main/java/com/ruhul/teamproject_practice/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //name added
+        System.out.print("my name is ruhul ");
     }
 }
